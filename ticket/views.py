4766c8caeeed9301dbd1055c634fab0989a8f6bc
GinoSunset <ginosunset@yandex.ru>
2022-12-27 17:15:38 +0300

    feat: add logout

--- ticket/views.py
+++ ticket/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Ticket
 
 
-class TicketsListView(ListView):
+class TicketsListView(LoginRequiredMixin, ListView):
     model = Ticket
     ordering = "-date_create"
