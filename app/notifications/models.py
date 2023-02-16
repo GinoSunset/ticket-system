78b5de73c20a8eab00c354843218472ed1ee6e04
GinoSunset <ginosunset@yandex.ru>
2023-02-16 20:54:56 +0300

    feat: change notify set contractor

--- app/notifications/models.py
+++ app/notifications/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
 from django.conf import settings
+from django.template import loader
 
 from ticket.models import Ticket
 
@@ -27,7 +28,7 @@ class Notification(models.Model):
     @classmethod
     def create_notify_update_ticket(cls, update_field, ticket: Ticket):
         template_dict = {
-            "contractor": Notification.create_notify_update_customer,
+            "contractor": Notification.create_notify_update_contractor,
         }
         for field in update_field:
             create_func = template_dict.get(field)
@@ -35,15 +36,10 @@ class Notification(models.Model):
                 create_func(ticket)
 
     @classmethod
-    def create_notify_update_customer(cls, ticket: Ticket):
+    def create_notify_update_contractor(cls, ticket: Ticket):
         link = f"{settings.PROTOCOL}://{Site.objects.get_current()}{ticket.get_absolute_url()}"
-        message = (
-            f"Вы назначены исполнителем заявки №{ticket.id}.\n"
-            "Информация о заявке:\n"
-            f"адрес: {ticket.address}\n"
-            "описание:\n"
-            f"{ticket.description} \n"
-            f"Перейти к заявке: {link}"
+        message = loader.get_template("notifications/notify_set_contractor.txt").render(
+            {"ticket": ticket, "link": link}
         )
         user = ticket.contractor
         cls.objects.create(user=user, message=message)
