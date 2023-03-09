c485482adc5f3f7db294e09211ab2efc0d94a526
GinoSunset <ginosunset@yandex.ru>
2023-03-09 22:07:15 +0300

    feat: append filter and list to admin notify

--- app/notifications/admin.py
+++ app/notifications/admin.py
@@ -3,7 +3,10 @@ from notifications.models import Notification
 
 
 class NotificationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("pk", "user", "message", "is_read", "type_notify")
+    readonly_fields = ("created_at",)
+    list_filter = ("is_read", "type_notify")
+    search_fields = ("message", "user__username", "user__email")
 
 
 admin.site.register(Notification, NotificationAdmin)
