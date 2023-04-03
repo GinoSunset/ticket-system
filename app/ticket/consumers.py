a7653dbbe176689404417c2a84194b7b6ea27bcc
GinoSunset <ginosunset@yandex.ru>
2023-04-03 16:26:20 +0300

    fix: admin user add to operator web channel

--- app/ticket/consumers.py
+++ app/ticket/consumers.py
@@ -18,7 +18,7 @@ class MainTableConsumer(AsyncJsonWebsocketConsumer):
             self.user = await database_sync_to_async(user.get_role_user)()
             self.user_id = self.user.pk
 
-            if self.user.is_operator:
+            if self.user.is_operator or self.user.is_admin:
                 await self.channel_layer.group_add(f"operators", self.channel_name)
                 return
             if self.user.is_customer:
@@ -40,6 +40,9 @@ class MainTableConsumer(AsyncJsonWebsocketConsumer):
         )({"ticket": ticket})
         ticket_json = {"ticket": render_ticket}
         await self.send_json(ticket_json)
+
+    async def hello(self, event):
+        await self.send_json(event)
 
     @database_sync_to_async
     def get_ticket_by_id(self, ticket_id):
