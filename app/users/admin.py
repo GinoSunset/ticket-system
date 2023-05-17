5dfa8e2ee813345816ef3a10c7d937e96d9c3eb2
GinoSunset <ginosunset@yandex.ru>
2023-05-17 21:16:59 +0300

    feat: add user_token to admin user view

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,5 +1,7 @@
+from typing import Any, List, Optional, Tuple, Union
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.http.request import HttpRequest
 from .models import User, Operator, Customer, Contractor, CustomerProfile
 from django.utils.translation import gettext_lazy as _
 
@@ -8,18 +10,28 @@ class UserAdmin(BaseUserAdmin):
     fieldsets = BaseUserAdmin.fieldsets + (
         (
             "Дополнительная информация",
-            {"fields": ("avatar", "phone", "role", "telegram_id")},
+            {
+                "fields": (
+                    "avatar",
+                    "phone",
+                    "role",
+                    "token",
+                )
+            },
         ),
-        ("Оповещения", {"fields": ("email_notify", "telegram_notify")}),
+        ("Оповещения", {"fields": ("email_notify", "telegram_id", "telegram_notify")}),
     )
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
         (
             "Дополнительная информация",
-            {"fields": ("avatar", "phone", "role", "telegram_id")},
+            {"fields": ("avatar", "phone", "role", "token")},
         ),
-        ("Оповещения", {"fields": ("email_notify", "telegram_notify")}),
+        ("Оповещения", {"fields": ("email_notify", "telegram_id", "telegram_notify")}),
     )
     list_filter = BaseUserAdmin.list_filter + ("role",)
+
+    def get_readonly_fields(self, request, obj=None):
+        return list(super().get_readonly_fields(request, obj)) + ["token"]
 
 
 class ProfileInline(admin.TabularInline):
@@ -39,12 +51,15 @@ class CustomerAdmin(BaseUserAdmin):
     list_display = ("username", "email", "phone")
     search_fields = ("username", "email", "phone")
 
+    def get_readonly_fields(self, request, obj=None):
+        return list(super().get_readonly_fields(request, obj)) + ["token"]
+
     fieldsets = BaseUserAdmin.fieldsets + (
-        ("Дополнительная информация", {"fields": ("avatar", "phone", "role")}),
+        ("Дополнительная информация", {"fields": ("avatar", "phone", "role", "token")}),
         ("Оповещения", {"fields": ("email_notify", "telegram_notify")}),
     )
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ("Дополнительная информация", {"fields": ("avatar", "phone", "role")}),
+        ("Дополнительная информация", {"fields": ("avatar", "phone", "role", "token")}),
         ("Оповещения", {"fields": ("email_notify", "telegram_notify")}),
     )
     list_filter = BaseUserAdmin.list_filter + ("role",)
