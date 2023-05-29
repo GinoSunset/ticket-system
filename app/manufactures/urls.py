c3f939b34a9a920651014723e6ed69b9545e3df6
GinoSunset <ginosunset@yandex.ru>
2023-05-29 21:23:40 +0300

    feat: add form create client and manuf

--- app/manufactures/urls.py
+++ app/manufactures/urls.py
@@ -4,4 +4,7 @@ import manufactures.views as views
 
 urlpatterns = [
     path("", views.ManufacturesListView.as_view(), name="manufactures-list"),
+    path("create/", views.ManufactureCreateView.as_view(), name="manufactures-create"),
+    path("<int:pk>/", views.ManufactureUpdateView.as_view(), name="manufacture-update"),
+    path("client/create/", views.ClientCreateView.as_view(), name="client-create"),
 ]
--- app/manufactures/views.py
+++ app/manufactures/views.py
@@ -1,8 +1,30 @@
-from django.views.generic import ListView
-from .models import Manufacture
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Manufacture, Client
 
 
 class ManufacturesListView(ListView):
     model = Manufacture
-    template_name = "manufactures_list.html"
     context_object_name = "manufactures"
+
+
+class ManufactureCreateView(CreateView):
+    model = Manufacture
+    fields = ["status", "client", "date_shipment", "branding", "comment"]
+    success_url = reverse_lazy("manufactures-list")
+
+    def form_valid(self, form):
+        form.instance.operator = self.request.user
+        return super().form_valid(form)
+
+
+class ManufactureUpdateView(UpdateView):
+    model = Manufacture
+    fields = ["status", "client", "date_shipment", "branding", "comment"]
+    success_url = reverse_lazy("manufactures-list")
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ["name", "comment"]
+    success_url = reverse_lazy("manufactures-create")
