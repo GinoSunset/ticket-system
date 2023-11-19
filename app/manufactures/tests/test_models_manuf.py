ec01970680b85fea3cf574c6d787340668472df1
GinoSunset <ginosunset@yandex.ru>
2023-11-19 15:01:01 +0300

    Refactor test_create_manuf_with_nomenclatures in test_models_manuf.py

--- app/manufactures/tests/test_models_manuf.py
+++ app/manufactures/tests/test_models_manuf.py
@@ -11,8 +11,8 @@ def test_create_manuf(manufacture_factory):
 @pytest.mark.django_db
 def test_create_manuf_with_nomenclatures(manufacture_factory, nomenclature_factory):
     batch_ncs = 10
-    ncs = nomenclature_factory.create_batch(batch_ncs)
-    manuf = manufacture_factory(nomenclatures=ncs)
+    manuf = manufacture_factory()
+    nomenclature_factory.create_batch(batch_ncs, manufacture=manuf)
     assert manuf.id is not None
     assert manuf.nomenclatures.count() == batch_ncs
 
