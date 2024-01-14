3d20e7a0684f8c7128b3e21e2e6ab821f3c7e07f
GinoSunset <ginosunset@yandex.ru>
2024-01-14 22:08:46 +0300

    feat: add operator group to add operators access to users

--- app/storage/tests/test_component_type.py
+++ app/storage/tests/test_component_type.py
@@ -12,7 +12,8 @@ def test_create_type_component(admin_client, component_type):
     data = {
         "name": "Test Component Type",
         "is_internal": False,
-        "parent_component_type": [component_type.pk],
+        "parents-TOTAL_FORMS": 0,
+        "parents-INITIAL_FORMS": "0",
     }
 
     response = admin_client.post(url_com_type_create, data)
@@ -23,41 +24,14 @@ def test_create_type_component(admin_client, component_type):
 
 
 @pytest.mark.django_db
-def test_create_type_component_with_sub_component(admin_client):
-    start_count = ComponentType.objects.count()
-
-    component = ComponentType.objects.create(
-        name="Test Component Type", is_internal=False
-    )
-    data = {
-        "name": "Test Component Type 2",
-        "is_internal": False,
-        "parent_component_type": component.pk,
-    }
-    response = admin_client.post(url_com_type_create, data)
-    assert response.status_code == 302
-    assert ComponentType.objects.count() - start_count == 2
-    assert (
-        ComponentType.objects.get(name="Test Component Type 2")
-        .parent_component_type.get()
-        .pk
-        == component.pk
-    )
-
-
-@pytest.mark.django_db
 def test_create_component_type_with_count(admin_client, component_type_factory):
     parent_component_type = component_type_factory()
     data = {
         "name": "SunComponent",
         "parents-0-parent_component_type": parent_component_type.pk,
         "parents-0-count_sub_components": 2,
-        "parents-TOTAL_FORMS":1,
-        'parents-TOTAL_FORMS':'1',
-        'parents-INITIAL_FORMS':'0',
-        'parents-MIN_NUM_FORMS':'0',
-        'parents-MAX_NUM_FORMS':'1000',
-
+        "parents-TOTAL_FORMS": "1",
+        "parents-INITIAL_FORMS": "0",
     }
     response = admin_client.post(url_com_type_create, data)
     assert response.status_code == 302
