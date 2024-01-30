1135935e5fc8a7a3cf077f8fac54fe143963c8c6
GinoSunset <ginosunset@yandex.ru>
2024-01-30 19:10:29 +0300

    feat: add search to client in manufacture

--- app/manufactures/forms.py
+++ app/manufactures/forms.py
@@ -18,7 +18,7 @@ class ManufactureForm(forms.ModelForm):
         widgets = {
             "date_shipment": CalendarInput(),
             "comment": forms.Textarea(attrs={"rows": 1, "placeholder": "Комментарий"}),
-            "client": forms.Select(attrs={"class": "ui selection dropdown"}),
+            "client": forms.Select(attrs={"class": "ui selection dropdown search"}),
         }
 
     def __init__(self, *args, **kwargs):
